df0b1fdbc68c41b5b330edd33206f17b0626ef4c
BizotChen <flightchen2015@gmail.com>
2023-02-20 20:53:11 +0800

    Update blocks.js

--- xlan/blocks.js
+++ xlan/blocks.js
@@ -1,7 +1,7 @@
-Blockly.Blocks['blinkled'] = {
+Blockly.Blocks['initled'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Blink a LED at pin")
+        .appendField("Initial a LED at pin")
         .appendField(new Blockly.FieldDropdown([["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"]]), "value_pin");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -10,11 +10,10 @@ Blockly.Blocks['blinkled'] = {
  this.setHelpUrl("");
   }
 };
-
-Blockly.Blocks['initled'] = {
+Blockly.Blocks['blinkled'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Initial a LED at pin")
+        .appendField("Blink a LED at pin")
         .appendField(new Blockly.FieldDropdown([["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"]]), "value_pin");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
