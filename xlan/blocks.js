78cf0a174d761e9b29abc71f753139497e92bb2a
BizotChen <flightchen2015@gmail.com>
2023-02-20 16:27:08 +0800

    fix bugs

--- xlan/blocks.js
+++ xlan/blocks.js
@@ -1,17 +1,12 @@
-Blockly.Blocks["test"] = {
- init: function() {
-	this.appendValueInput("pin")
-		.setCheck("Number")
-		.appendField("digitalWrite")
-		.appendField("pin");
-	this.appendValueInput("val")
-		.setCheck("Number")
-		.appendField("value");
-	this.setInputsInline(true);
-	this.setPreviousStatement(true, null);
-	this.setNextStatement(true, null);
-	this.setColour(Blockly.Msg["MYBLOCKS_HUE"]);
-   this.setTooltip("Blocks definition");
-   this.setHelpUrl("https://developers.google.com/blockly/guides/create-custom-blocks/overview");
+Blockly.Blocks['blink'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Blink LED at pin")
+        .appendField(new Blockly.FieldDropdown([["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"]]), "value_pin");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
   }
 };
--- xlan/javascript.js
+++ xlan/javascript.js
@@ -1,11 +1,10 @@
-Blockly.Arduino['test'] = function(block) {
+Blockly.Arduino['blink'] = function(block) {
   //Blockly.Arduino.definitions_['name'] = '//definitions_';
   //Blockly.Arduino.setups_['name'] = '//setups_';
   //Blockly.Arduino.setups_bottom_['name'] = '//setups_bottom_';
   //Blockly.Arduino.functions_['name'] = 'String blockly() {\n  return "Hello World";\n}';
 
-  var value_pin = Blockly.Arduino.valueToCode(block, 'pin', Blockly.Arduino.ORDER_ATOMIC);
-  var value_val = Blockly.Arduino.valueToCode(block, 'val', Blockly.Arduino.ORDER_ATOMIC);
-  var code = 'digitalWrite(%1, %2);\n'.replace('%1',value_pin).replace('%2',value_val);
+  var value_pin = block.getFieldValue('value_pin');
+  var code = 'digitalWrite(%1, LOW);\ndelay(1000);\ndigitalWrite(%1, HIGH);\ndelay(1000);\n'.replace('%1',value_pin);
   return code;
 };
