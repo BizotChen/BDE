162a4dab503a915e5c4b5eeb5c64a8d517996a58
BizotChen <flightchen2015@gmail.com>
2023-02-20 21:01:56 +0800

    fix bugs

--- xlan/javascript.js
+++ xlan/javascript.js
@@ -1,10 +1,10 @@
-Blockly.JavaScript['blinkled'] = function(block) {
-  var value_pin = block.getFieldValue('value_pin');
-  var code = 'digitalWrite(%1, LOW);\ndelay(1000);\ndigitalWrite(%1, HIGH);\ndelay(1000);\n'.replace('%1',value_pin).replace('%1',value_pin);
-  return code;
+Blockly.JavaScript['initled'] = function(block) {
+  var init_led_pin = block.getFieldValue('init_led_pin');
+  Blockly.Arduino.setups_['init_led_setup'] = 'pinMode(%1, OUTPUT);\n'.replace('%1', init_led_pin);
 };
 
-Blockly.JavaScript['initled'] = function(block) {
-  var value_pin = block.getFieldValue('value_pin');
-  Blockly.Arduino.setups_['init_led_setup'] = 'pinMode(%1, OUTPUT);\n'.replace('%1', value_pin);
+Blockly.JavaScript['blinkled'] = function(block) {
+  var blink_led_pin = block.getFieldValue('blink_led_pin');
+  var code = 'digitalWrite(%1, LOW);\ndelay(1000);\ndigitalWrite(%1, HIGH);\ndelay(1000);\n'.replace('%1',value_pin).replace('%1',blink_led_pin);
+  return code;
 };
