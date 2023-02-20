66795e9e3989983aec48433d9800f757a26c3971
BizotChen <flightchen2015@gmail.com>
2023-02-20 20:47:26 +0800

    fix bugs

--- xlan/blocks.js
+++ xlan/blocks.js
@@ -1,7 +1,20 @@
-Blockly.Blocks['blink'] = {
+Blockly.Blocks['blinkled'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Blink LED at pin")
+        .appendField("Blink a LED at pin")
+        .appendField(new Blockly.FieldDropdown([["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"]]), "value_pin");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['initled'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Initial a LED at pin")
         .appendField(new Blockly.FieldDropdown([["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"]]), "value_pin");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
--- xlan/javascript.js
+++ xlan/javascript.js
@@ -1,11 +1,11 @@
-Blockly.Arduino['blink'] = function(block) {
+Blockly.JavaScript['blinkled'] = function(block) {
   var value_pin = block.getFieldValue('value_pin');
-
-  //Blockly.Arduino.definitions_['name'] = '//definitions_';
-  Blockly.Arduino.setups_['blink_setup'] = 'pinMode(%1, OUTPUT);\n'.replace('%1', value_pin);
-  //Blockly.Arduino.setups_bottom_['name'] = '//setups_';
-  //Blockly.Arduino.functions_['name'] = 'String blockly() {\n  return "Hello World";\n}';
-
   var code = 'digitalWrite(%1, LOW);\ndelay(1000);\ndigitalWrite(%1, HIGH);\ndelay(1000);\n'.replace('%1',value_pin).replace('%1',value_pin);
+  return code;
+};
+
+Blockly.JavaScript['initled'] = function(block) {
+  var value_pin = block.getFieldValue('value_pin');
+  Blockly.Arduino.setups_['init_led_setup'] = 'pinMode(%1, OUTPUT);\n'.replace('%1', value_pin);
   return code;
 };
