b2b49b795d0e116f38d722c90339d4db7ceb5279
BizotChen <flightchen2015@gmail.com>
2023-02-21 10:58:22 +0800

    fix bugs

--- xlan/blocks.js
+++ xlan/blocks.js
@@ -2,7 +2,7 @@ Blockly.Blocks['initled'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Initial a LED at pin")
-        .appendField(new Blockly.FieldDropdown([["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"]]), "init_led_pin");
+        .appendField(new Blockly.FieldDropdown([["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"]]), "pin");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
@@ -14,7 +14,7 @@ Blockly.Blocks['blinkled'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Blink a LED at pin")
-        .appendField(new Blockly.FieldDropdown([["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"]]), "blink_led_pin");
+        .appendField(new Blockly.FieldDropdown([["2","2"], ["3","3"], ["4","4"], ["5","5"], ["6","6"], ["7","7"], ["8","8"], ["9","9"], ["10","10"], ["11","11"], ["12","12"], ["13","13"]]), "pin");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
--- xlan/javascript.js
+++ xlan/javascript.js
@@ -1,12 +1,12 @@
 Blockly.Arduino['initled'] = function(block) {
-  var ddl_pin = block.getFieldValue('init_led_pin');
+  var ddl_pin = block.getFieldValue('pin');
   Blockly.Arduino.setups_['pinmode_' + ddl_pin] = 'pinMode(' + ddl_pin + ', OUTPUT);\n';
-  var code = ''
+  var code = '';
   return code;
 };
 
 Blockly.Arduino['blinkled'] = function(block) {
-  var ddl_pin = block.getFieldValue('blink_led_pin');
+  var ddl_pin = block.getFieldValue('pin');
   var code = 'digitalWrite(' + ddl_pin + ', LOW);\ndelay(1000);\ndigitalWrite(' + ddl_pin + ', HIGH);\ndelay(1000);\n';
   return code;
 };
